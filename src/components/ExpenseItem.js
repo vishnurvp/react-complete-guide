c81955000b5e27ba08267234956ebc230c498b1f
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-25 14:04:09 +0530

    React_Task7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+// component in react js is just a javascript function
+function ExpenseItem() {
+    return (<div>
+        <h2>Expense Item</h2>
+        <h3>Food Rs 10</h3>
+        <h3>Petrol Rs 100</h3>
+        <h3>Movies Rs 200</h3>
+        </div>)
+}
+
+export default ExpenseItem;
