93bb888518a7c465200e96894934e7ade67de972
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-27 14:55:59 +0530

    Module2_Task2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 // component in react js is just a javascript function
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import ExpenseDate from "./ExpenseDate";
@@ -8,12 +9,20 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // should return only one root element
 
+  const [title, setTitle] = useState(props.title); // [varable, updatefunc]
+  const [amount, setAmount] = useState(props.amount);
+  
   const changeTitleClickHandler = () => {
-    console.log('Change Title clicked');
+    setTitle('Updated!');
+    console.log(title);
   }
 
   const deleteClickHandler = () => {
     console.log('Delete Expense clicked');
+  }
+
+  const changeExpenseClickHandler = () => {
+    setAmount(1000);
   }
 
   return (
@@ -21,11 +30,12 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         location={props.location}
-        title={props.title}
-        amount={props.amount}
+        title={title}
+        amount={amount}
       />
       <button onClick={changeTitleClickHandler}>Change Title</button>
       <button onClick={deleteClickHandler}>Delete Expense</button>
+      <button onClick={changeExpenseClickHandler}>Change Expense to 1000 Rs</button>
     </Card>
   );
 }
