31b6bc12aecd0cf49c0d9bf0ef541b041747806f
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-28 15:35:29 +0530

    Module2_Task8

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 // component in react js is just a javascript function
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import ExpenseDate from "./ExpenseDate";
@@ -9,33 +9,33 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // should return only one root element
 
-  const [title, setTitle] = useState(props.title); // [varable, updatefunc]
-  const [amount, setAmount] = useState(props.amount);
+  // const [title, setTitle] = useState(props.title); // [varable, updatefunc]
+  // const [amount, setAmount] = useState(props.amount);
   
-  const changeTitleClickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  }
+  // const changeTitleClickHandler = () => {
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // }
 
-  const deleteClickHandler = () => {
-    console.log('Delete Expense clicked');
-  }
+  // const deleteClickHandler = () => {
+  //   console.log('Delete Expense clicked');
+  // }
 
-  const changeExpenseClickHandler = () => {
-    setAmount(1000);
-  }
+  // const changeExpenseClickHandler = () => {
+  //   setAmount(1000);
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         location={props.location}
-        title={title}
-        amount={amount}
+        title={props.title}
+        amount={props.amount}
       />
-      <button onClick={changeTitleClickHandler}>Change Title</button>
+      {/* <button onClick={changeTitleClickHandler}>Change Title</button>
       <button onClick={deleteClickHandler}>Delete Expense</button>
-      <button onClick={changeExpenseClickHandler}>Change Expense to 1000 Rs</button>
+      <button onClick={changeExpenseClickHandler}>Change Expense to 1000 Rs</button> */}
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,28 @@
+import React, {useState} from 'react';
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
   return (
     <Card className="expenses">
-      {props.items.map((expense) => {
-        return (
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {props.items.map((expense) => 
           <ExpenseItem
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
             location={expense.location}
           />
-        );
-      })}
+        )
+      }
     </Card>
   );
 };
