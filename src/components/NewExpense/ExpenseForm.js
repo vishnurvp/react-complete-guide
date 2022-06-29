3766e5d98b8c839cd6e3b13fa892375084d5a1db
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-29 12:37:49 +0530

    Module2_Task12

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,7 +41,7 @@ const ExpenseForm = (props) => {
 
   const locationChangeHandler = (event) => {
     setEnteredLocation(event.target.value);
-  }
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -49,7 +49,7 @@ const ExpenseForm = (props) => {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
-      location: enteredLocation
+      location: enteredLocation,
     };
     props.onSubmitExpenseData(expenseData);
     setEnteredTitle("");
@@ -99,6 +99,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
