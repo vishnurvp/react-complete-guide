1321b50ac2bb03917a05af02a73ce846532d13a8
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-29 11:19:48 +0530

    Module2_Task10

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,13 +12,17 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return (expense.date.getFullYear().toString() === filteredYear);
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
